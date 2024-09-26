ae2e7bcf6c05bf9debcaa81ed48c92f365f9e847
ismarwonder <150816908+ismarwonder@users.noreply.github.com>
2024-09-27 00:35:20 +0200

    hide firebase intel

--- script.js
+++ script.js
@@ -13,16 +13,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 // Firebase-konfiguration
-const firebaseConfig = {
-    apiKey: "AIzaSyCEnhv-697OiE56F0ghAuhSMh1wxm-ppnM",
-    authDomain: "klassflagg.firebaseapp.com",
-    projectId: "klassflagg",
-    databaseURL: "https://klassflagg-default-rtdb.europe-west1.firebasedatabase.app/",
-    storageBucket: "klassflagg.appspot.com",
-    messagingSenderId: "85254934938",
-    appId: "1:85254934938:web:830f4b9e1e078566e5aa23",
-    measurementId: "G-5Z0Y08ZQYB"
-  };
+import { firebaseConfig } from './config.js';
 
 // Initialisera Firebase
 const app = initializeApp(firebaseConfig);
