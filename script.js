6c6b8c7d47974f47c0db7fe8958a09b9bc644bf1
ismarwonder <150816908+ismarwonder@users.noreply.github.com>
2024-09-27 00:52:56 +0200

    Revert "hide firebase intel"
    This reverts commit ae2e7bcf6c05bf9debcaa81ed48c92f365f9e847.

--- script.js
+++ script.js
@@ -13,7 +13,16 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 // Firebase-konfiguration
-import { firebaseConfig } from './config.js';
+const firebaseConfig = {
+    apiKey: "AIzaSyCEnhv-697OiE56F0ghAuhSMh1wxm-ppnM",
+    authDomain: "klassflagg.firebaseapp.com",
+    projectId: "klassflagg",
+    databaseURL: "https://klassflagg-default-rtdb.europe-west1.firebasedatabase.app/",
+    storageBucket: "klassflagg.appspot.com",
+    messagingSenderId: "85254934938",
+    appId: "1:85254934938:web:830f4b9e1e078566e5aa23",
+    measurementId: "G-5Z0Y08ZQYB"
+  };
 
 // Initialisera Firebase
 const app = initializeApp(firebaseConfig);
