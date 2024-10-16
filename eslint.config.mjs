2a2729e9dad11de84a3f8fa6539bf4df1c1634ad
ismarwonder <150816908+ismarwonder@users.noreply.github.com>
2024-10-16 11:49:17 +0200

    installed eslint prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   {
+    plugins: {
+      prettier: prettierPlugin
+    },
+    extends: [
+      'plugin:prettier/recommended'
+    ],
     rules: {
-        "no-unused-vars": "warn",
-        "no-undef": "warn"
+      "prettier/prettier": "error",
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
     }
   }
 ];
